f8ba9ed79d93bccb5207694418fea9bbd964ecb0
Vincent Paturet <vincent.paturet@entur.org>
2025-06-25 19:10:58 +0200

    Configure testcontainers

--- src/test/java/org/rutebanken/irkalla/routes/RouteBuilderIntegrationTestBase.java
+++ src/test/java/org/rutebanken/irkalla/routes/RouteBuilderIntegrationTestBase.java
@@ -18,23 +18,69 @@ package org.rutebanken.irkalla.routes;
 import org.apache.camel.CamelContext;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.apache.camel.test.spring.junit5.UseAdviceWith;
+import org.entur.pubsub.base.EnturGooglePubSubAdmin;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PubSubEmulatorContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @CamelSpringBootTest
 @UseAdviceWith
-@ActiveProfiles({"default",  "in-memory-blobstore", "google-pubsub-emulator", "test","google-pubsub-autocreate", "no-kafka"})
+@ActiveProfiles({"default",  "in-memory-blobstore", "test","google-pubsub-autocreate", "no-kafka"})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public abstract class RouteBuilderIntegrationTestBase {
+
+    private static PubSubEmulatorContainer pubsubEmulator;
+
+    @Autowired
+    private EnturGooglePubSubAdmin enturGooglePubSubAdmin;
 
     @Autowired
     protected CamelContext context;
 
+    @BeforeAll
+    public static void init() {
+        pubsubEmulator =
+                new PubSubEmulatorContainer(
+                        DockerImageName.parse(
+                                "gcr.io/google.com/cloudsdktool/cloud-sdk:emulators"
+                        )
+                );
+        pubsubEmulator.start();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        pubsubEmulator.stop();
+    }
+
+
     @AfterEach
     void stopContext() {
         context.stop();
+    }
+
+    @AfterEach
+    public void teardown() {
+        enturGooglePubSubAdmin.deleteAllSubscriptions();
+    }
+
+    @DynamicPropertySource
+    static void emulatorProperties(DynamicPropertyRegistry registry) {
+        registry.add(
+                "spring.cloud.gcp.pubsub.emulator-host",
+                pubsubEmulator::getEmulatorEndpoint
+        );
+        registry.add(
+                "camel.component.google-pubsub.endpoint",
+                pubsubEmulator::getEmulatorEndpoint
+        );
     }
 
 }
--- src/test/java/org/rutebanken/irkalla/routes/notification/EntityChangedRouteBuilderTest.java
+++ src/test/java/org/rutebanken/irkalla/routes/notification/EntityChangedRouteBuilderTest.java
@@ -17,7 +17,7 @@ import java.time.Instant;
 
 @SpringBootTest
 @CamelSpringBootTest
-@ActiveProfiles(value = {"default",  "in-memory-blobstore", "google-pubsub-emulator", "test","google-pubsub-autocreate"}, inheritProfiles = false)
+@ActiveProfiles(value = {"default", "in-memory-blobstore", "test", "google-pubsub-autocreate"}, inheritProfiles = false)
 public class EntityChangedRouteBuilderTest extends RouteBuilderIntegrationTestBase {
 
     @Produce("google-pubsub:{{irkalla.pubsub.project.id}}:ror.tiamat.changelog")
